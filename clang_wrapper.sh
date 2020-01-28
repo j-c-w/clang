218a9ee1061b48889b92bdc1b076cc62106adc90
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-01-28 18:02:18 +0000

    Reduce timeout for clang wrapper to 20 seconds

--- clang_wrapper.sh
+++ clang_wrapper.sh
@@ -6,4 +6,4 @@ execdir=$(dirname $0)
 
 idl_clang="$execdir/idlclang"
 
-(timeout 300 $idl_clang "$@" && echo "****IDLClang Worked :)") || (echo "****IDLClang Failed.  Falling back to normal clang"; exec $idl_clang "$@" -fno-idl)
+(timeout 20 $idl_clang "$@" && echo "****IDLClang Worked :)") || (echo "****IDLClang Failed.  Falling back to normal clang"; exec $idl_clang "$@" -fno-idl)
