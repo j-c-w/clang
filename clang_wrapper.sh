179c61deba52a0ec133a7291148eafa51e381519
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-01-29 11:33:47 +0000

    Clang wrapper now prints if there was a timeout

--- clang_wrapper.sh
+++ clang_wrapper.sh
@@ -6,4 +6,13 @@ execdir=$(dirname $0)
 
 idl_clang="$execdir/idlclang"
 
-(timeout 20 $idl_clang "$@" && echo "****IDLClang Worked :)") || (echo "****IDLClang Failed.  Falling back to normal clang"; exec $idl_clang "$@" -fno-idl)
+(timeout 8 $idl_clang "$@" && echo "****IDLClang Worked :)")
+err_code=$?
+
+if [[ $err_code == 124 ]]; then
+  echo "****IDLClang Timed out.  Falling back to normal clang"
+  exec $idl_clang "$@" -fno-idl
+elif [[ $err_code -ne 0 ]]; then
+  echo "****IDLClang Failed.  Falling back to normal clang"
+  exec $idl_clang "$@" -fno-idl
+fi
