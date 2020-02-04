2a516b1839b0efb5e1292dcb2edf6a8657d597d7
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-02-04 18:08:30 +0000

    Add a grep for matching matrixes

--- clang_wrapper.sh
+++ clang_wrapper.sh
@@ -6,6 +6,16 @@ execdir=$(dirname $0)
 
 idl_clang="$execdir/idlclang"
 
+# Grep every file for 'matrix'
+echo "Scanning started"
+for arg in "$@"; do
+  if [[ -f $arg ]]; then
+    if [[ $arg != *.o ]] && [[ $arg != *.so ]] && [[ $arg != *.a ]]; then
+      grep -i matrix $arg
+    fi
+  fi
+done
+
 (timeout 8 $idl_clang "$@" && echo "****IDLClang Worked :)")
 err_code=$?
 
